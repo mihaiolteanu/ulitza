8121454dca8e1c8fa24a2cb79e1898c9672d7ba3
mihaiolteanu <mihai_olteanu@fastmail.fm>
2023-08-10 10:28:14 +0300

    Update isle of man

--- affixes.js
+++ affixes.js
@@ -68,7 +68,7 @@ const affixes = {
   iraq                   : /^(شارع|شەقامی|جادەیا|ڕێگای) | (boulevard|road|street)$/i,
   "ireland-and-northern-ireland": /^(the) |('s|) (street|road|drive|avenue|square|hill|terrace|place|lane|view|close|park|grove|walk|court|cove|crescent)$|(view)$/i,
   italy                  : /^(viale|via|corso|piazzale|piazzetta|piazza|lungomare|autostrada|strada|traversa|largo|contrada|salita|vicolo|vico|monte|campo|borgo) (provinciale |statale |)([0-9]* |)(dell'|della |delle |dello |degli |del |dei |di |al |)| (sud|nord|vecchia|antica)$/ig,
-  "isle-of-man"          : / (road|promenade|street|main)$/i,
+  "isle-of-man"          : / (main |)(road|promenade|street|main|square|avenue|park)$/i,
   "israel-and-palestine" : /^(نهج|شارع|طريق|שדרות)/i,  
   "ivory-coast"          : /^(boulevard|avenue|rue) (de la |des |de |du |du |)/i,
   jordan                 : /^(شارع الشهيد|شارع|الشارع)| (street|highway)$/i,
