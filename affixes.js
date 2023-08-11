5ff77b6b60aa8d339a500a7725575003f813e832
mihaiolteanu <mihai_olteanu@fastmail.fm>
2023-08-11 14:54:36 +0300

    Update Serbia

--- affixes.js
+++ affixes.js
@@ -112,7 +112,7 @@ const affixes = {
   russia                 : /^(улица|переулок|проезд|проспект|Площадь|бульвар|Большая|(улица |дорога |аллея )([1-9])-(я|й|го|е) )|^[0-9]+-.*|(ская улица|ский переулок|ский проезд|улица|переулок|проезд|проспект|Площадь| бульвар| шоссе| тракт)$/ig,
   scotland               : / (street|road)$/i,
   "senegal-and-gambia"   : /^(rue|route|avenue|boulevard) (des |de |)| (road|highway|avenue)$|[a-z][a-z]-[0-9]+$/ig,
-  serbia                 : /^(Трг|Булевар) | (пут|друм|ulica)$/i,
+  serbia                 : /^(Трг|Булевар|Алеја|Пут|Парк) | (пут|друм|ulica|трг|булевар)$/i,
   slovakia               : /^(námestie|pri|pod|nad|na|za) |(ova| námestie| rad| utca| cesta)$/i,
   slovenia               : /^(ulica|trg|pod|cesta|na) | (ulica|cesta|odreda|pot|trg|vrh)$/ig,
   "south-africa"         : /1st|2nd|3rd|4th|5th|6th|7th|8th|11th|12th|east|north|west|south|(road|street|drive|avenue|straat)$/ig,
